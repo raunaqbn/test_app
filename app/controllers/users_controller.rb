87f420644dc7e3407cfc93aefd8c4d67266b8b5b
Raunaq <raunaq.naidu@gmail.com>
2015-12-27 21:48:27 +0000

    Added user show and index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
    def new
        @user = User.new
    end 
+   
+  def index
+    @users = User.all
+  end
+
    def create
      @user = User.new(user_params)
      if @user.save
@@ -11,6 +16,24 @@ class UsersController < ApplicationController
        render 'new'
      end
    end
+   
+   
+  def edit
+    @user = User.find(params[:id])
+  end
+ 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to testimonials_path
+    else
+      render 'edit'
+    end
+  end
+  def show
+    @user = User.find(params[:id])
+  end
 
 private
   def user_params
