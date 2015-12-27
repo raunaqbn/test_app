3dcca5a798dfd086f8e60a689b08ff85887792c1
Raunaq <raunaq.naidu@gmail.com>
2015-12-27 22:09:58 +0000

    changing heading to patients

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,6 @@ class UsersController < ApplicationController
    def new
        @user = User.new
    end 
-   
-  def index
-    @users = User.all
-  end
 
    def create
      @user = User.new(user_params)
@@ -34,6 +30,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+     
+  def index
+    @users = User.all
+  end
+
 
 private
   def user_params
