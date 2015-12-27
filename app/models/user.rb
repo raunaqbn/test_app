62f3aa961559d78d2840e4a59c2b726b78e04685
Raunaq <raunaq.naidu@gmail.com>
2015-12-27 04:41:13 +0000

    Added signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     has_many :testimonials
     before_save { self.email = email.downcase }
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: { minimum: 3, maximum: 25}
-        validates :privelige, presence: true, length: {maximum: 5}
+    #validates :privelige, presence: true, length: {maximum: 5}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false },format: { with: VALID_EMAIL_REGEX }
     has_secure_password
