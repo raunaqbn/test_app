dd3de5703b385425fa0439695cfe2e6721200eaa
Raunaq <raunaq.naidu@gmail.com>
2015-12-27 03:23:44 +0000

    Add authenticaiton for the users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151226220722) do
+ActiveRecord::Schema.define(version: 20151227030544) do
 
   create_table "testimonials", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20151226220722) do
     t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
