9e0c0492154f81c19c5358f346f580b115798cdd
Raunaq <raunaq.naidu@gmail.com>
2015-12-29 01:19:13 +0000

    Added admin user delete features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    has_many :testimonials
+    has_many :testimonials, dependent: :destroy
     before_save { self.email = email.downcase }
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: { minimum: 3, maximum: 25}
     #validates :privelige, presence: true, length: {maximum: 5}
